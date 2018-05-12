fe03ba836e3f14724f693cde075db5f24403e89e
DaniyarJakupov <ddr199305@gmail.com>
2018-05-12 20:12:42 +0200

    Create fallbackImg cache and respond with it if real img is not fetched

--- client/service-worker.js
+++ client/service-worker.js
@@ -1,17 +1,52 @@
-const counts = {
-  installs: 0,
-  activations: 0,
-  fetches: 0
-};
+const FALLBACK_IMG = 'https://localhost:3100/images/fallback-grocery.png';
 
+const fallbackImages = 'fallback-image'; // cache name
+
+/* INSTALL SERVICE WORKER EVENT LISTENER */
 self.addEventListener('install', event => {
-  console.log('installs', ++counts.installs);
+  // .waitUntil expects a Promise
+  // caches.open returns a Promise
+  event.waitUntil(
+    caches.open(fallbackImages).then(cache => {
+      cache.add(FALLBACK_IMG);
+    })
+  );
 });
-
+/* ===================================================== */
+/* ACTIVATE SERVICE WORKER EVENT LISTENER */
 self.addEventListener('activate', event => {
-  console.log('activations', ++counts.activations);
+  // REMOVE all caches except fallbackImages
+  // caches.keys().then(cacheNames => {
+  // })
 });
 
-// self.addEventListener('fetch', event => {
-//   console.log('fetches', event.request.url);
-// });
+/* ===================================================== */
+/* FETCH REQUESTS EVENT LISTENER */
+self.addEventListener('fetch', event => {
+  let acceptHeader = event.request.headers.get('accept');
+  let requestUrl = new URL(event.request.url);
+  if (
+    acceptHeader.indexOf('image/*') >= 0 &&
+    requestUrl.pathname.indexOf('/images/') === 0
+  ) {
+    event.respondWith(fetchImageOrFallback(event));
+  }
+});
+
+/* ===================================================== */
+/* HELPERS */
+function fetchImageOrFallback(fetchEvent) {
+  return fetch(fetchEvent.request, {
+    mode: 'cors'
+  })
+    .then(response => {
+      if (!response.ok) {
+        return caches.match(FALLBACK_IMG, { cacheName: fallbackImages });
+      } else {
+        return response;
+      }
+    })
+    .catch(() => {
+      return caches.match(FALLBACK_IMG, { cacheName: fallbackImages });
+    });
+}
