a8df8dea00db81427014e3533e1b3686def92e48
DaniyarJakupov <ddr199305@gmail.com>
2018-05-15 09:47:51 +0200

    Implement fetchAPIWithFallback and add comments

--- client/service-worker.js
+++ client/service-worker.js
@@ -7,16 +7,24 @@ import {
 
 const FALLBACK_IMG_URL = 'https://localhost:3100/images/fallback-grocery.png';
 
+/**
+ * WE HAVE 3 CACHES FOR DIFF TYPES OF DATA (check './sw/caches.js)
+ *  1. fallbackImages: cacheName('FALLBACK_IMAGES'), [fallback image]
+ *  2. prefetch: cacheName('PREFETCH'), [app.js, web-app-manifest.json, static img]
+ *  3. fallback: cacheName('FALLBACK') [api calls]
+ */
+
 /* INSTALL SERVICE WORKER EVENT LISTENER */
 self.addEventListener('install', event => {
   // .waitUntil expects a Promise
   event.waitUntil(
     Promise.all([
-      // get the fallback image
+      // Fetch fallback image and add it to fallbackImages cache
       caches.open(ALL_CACHES.fallbackImages).then(cache => {
+        // cache.add fetches provided url and stores data
         cache.add(FALLBACK_IMG_URL);
       }),
-      // populate the precache assets
+      // Fetch assets, then populate the prefetch cache
       precacheStaticAssets()
     ])
   );
@@ -30,49 +38,87 @@ self.addEventListener('activate', event => {
 
 /* ===================================================== */
 /* FETCH REQUESTS EVENT LISTENER */
-/* Implementation of diff cache startegies depending on a file type */
 self.addEventListener('fetch', event => {
   let acceptHeader = event.request.headers.get('accept');
   let requestUrl = new URL(event.request.url);
+
+  let isGroceryImage =
+    acceptHeader.indexOf('image/*') >= 0 &&
+    requestUrl.pathname.indexOf('/images/') === 0;
+
+  let isAPI = requestUrl.origin.indexOf('localhost:3100') >= 0;
+
+  /* Implementation of diff cache strategies depending on a file type */
   // .respondWith() takes a Promise
   event.respondWith(
     caches
+      // Firstly, check prefetch cache [app.js, web-app-manifest.json, static img]
       .match(event.request, { cacheName: ALL_CACHES.prefetch })
       .then(response => {
-        // If precache with assets is found, return it
+        // If assets are found, return them
         if (response) return response;
-        // Otherwise
-        if (acceptHeader.indexOf('image/*') >= 0) {
-          if (requestUrl.pathname.indexOf('/images/') === 0) {
-            return fetchImageOrFallback(event);
-          }
+        // If client requests images
+        if (isGroceryImage) {
+          return fetchImageOrFallback(event);
+        } else if (isAPI) {
+          // If client requests data from the server (json, etc...)
+          return fetchAPIWithFallback(event);
         }
-        // Else make a request to the server
+        // For everything else, make a request directly to the server
         return fetch(event.request);
       })
   );
 });
 
 /* ===================================================== */
-/* HELPERS */
-// Cache then Network Technique
+/* ======= HELPER FUNCTIONS (Cache then Network Technique) ======= */
+
+// ====== Caching images ======
 function fetchImageOrFallback(fetchEvent) {
+  // Fetch images from the server
   return fetch(fetchEvent.request, {
-    mode: 'cors',
-    credentials: 'omit' // in case CORS wildcard headers are present
+    mode: 'cors'
+    //credentials: 'omit' // in case CORS wildcard headers are present
   })
     .then(response => {
+      // IF response without image, go to fallbackImages cache
       if (!response.ok) {
         return caches.match(FALLBACK_IMG_URL, {
           cacheName: ALL_CACHES.fallbackImages
         });
       } else {
+        // Else, return image from the server
         return response;
       }
     })
     .catch(() => {
+      // IF network fails, go to fallbackImages cache
       return caches.match(FALLBACK_IMG_URL, {
         cacheName: ALL_CACHES.fallbackImages
       });
     });
+}
+
+// ====== Caching API responses (json, etc) ======
+/**
+ * @return {Promise<Response>}
+ */
+function fetchAPIWithFallback(fetchEvent) {
+  // Open fallback cache, then
+  return caches.open(ALL_CACHES.fallback).then(cache => {
+    // fetch what you need from the server
+    return fetch(fetchEvent.request)
+      .then(response => {
+        // Clone the response and put its copy to the cache
+        let clonedResponse = response.clone();
+        // cache.put() is not fetching again, more efficient than cache.add
+        cache.put(fetchEvent.request, clonedResponse);
+        // Resolve Promise with the original response
+        return response;
+      })
+      .catch(() => {
+        // IF network fails, serve from the cache
+        return cache.match(fetchEvent.request);
+      });
+  });
 }
